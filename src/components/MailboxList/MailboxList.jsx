a3b42f3b89a11825d7f5cc260d53a3817e0e95c5
rednaldeirf <jason.d.friedlander@gmail.com>
2025-04-18 09:13:59 -0400

    final lab

--- src/components/MailboxList/MailboxList.jsx
+++ src/components/MailboxList/MailboxList.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const MailboxList = ({ mailboxes }) => {
   return (
     <div>
+        <h1>Mailbox List</h1>
       {mailboxes.map((mailbox) => (
         <Link key={mailbox._id} to={`/mailboxes/${mailbox._id}`} className="mail-box">
           <div>Mailbox {mailbox._id}</div>
